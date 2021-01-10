187b8437da1f88db26e0b1c4afb3cb2b1f123ab3
bnayagrawal <bnayagrawal@gmail.com>
2021-01-10 11:09:35 +0530

    use java configuration.

---
+++ src/main/java/xyz/bnayagrawal/jokeapp/config/ChuckConfiguration.java
@@ -0,0 +1,17 @@
+package xyz.bnayagrawal.jokeapp.config;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/*
+* Java bean configuration
+* */
+@Configuration
+public class ChuckConfiguration {
+
+    @Bean
+    public ChuckNorrisQuotes chuckNorrisQuotes() {
+        return new ChuckNorrisQuotes();
+    }
+}
